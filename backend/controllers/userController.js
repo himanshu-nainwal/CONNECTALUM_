6975c31f186548525a8e872a3e4d72a8b842d9a9
himanshu-nainwal <himanshunainwal83@gmail.com>
2026-03-17 23:13:51 +0530

    Fix Vercel 500 crash by replacing bcrypt with pure JS bcryptjs

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
 import User from "../models/User.js";
 import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import validator from "validator";
 
 const createToken = (id, role) => {
